e1b95fad9eac74f2efa145dd72ba66e1ba529e2d
Kahloun <jean-loup.kahloun@invenietis.com>
2012-05-18 10:31:43 +0000

    - updating runtime, to use strongly-named commong.Logging, Commong.Logging.Log4Net and Log4net dlls

--- Output/ExtractionMacros/ExtractDebug.bat
+++ Output/ExtractionMacros/ExtractDebug.bat
@@ -1,6 +1,7 @@
 xcopy ..\..\CK.Windows.App\bin\Debug ..\Debug\ /y
 xcopy ..\..\CK.Windows.Config\bin\Debug ..\Debug\ /y
 xcopy ..\..\CK.Windows.Core\bin\Debug ..\Debug\ /y
-xcopy ..\..\CK.Windows.App\bin\Debug ..\Debug\ /y
+xcopy ..\..\CK.Windows.Demo\bin\Debug ..\Debug\ /y
 
-
+del ..\Debug\*.xml
+del ..\Debug\*.exe
--- Output/ExtractionMacros/ExtractRelease.bat
+++ Output/ExtractionMacros/ExtractRelease.bat
@@ -1,6 +1,7 @@
 xcopy ..\..\CK.Windows.App\bin\Release ..\Release\ /y
 xcopy ..\..\CK.Windows.Config\bin\Release ..\Release\ /y
 xcopy ..\..\CK.Windows.Core\bin\Release ..\Release\ /y
-xcopy ..\..\CK.Windows.App\bin\Release ..\Release\ /y
+xcopy ..\..\CK.Windows.Demo\bin\Release ..\Release\ /y
 
 del ..\Release\*.pdb
+del ..\Release\*.exe
