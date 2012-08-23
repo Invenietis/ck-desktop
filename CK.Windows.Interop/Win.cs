f4c2c5ef176a6e705895eb83d07d5d9b1d5adb38
Jean-Loup Kahloun <jean-loup.kahloun@invenietis.com>
2012-08-23 16:40:37 +0200

    Added the LGPL License on each .cs file

--- CK.Windows.Interop/Win.cs
+++ CK.Windows.Interop/Win.cs
@@ -1,6 +1,6 @@
 #region LGPL License
 /*----------------------------------------------------------------------------
-* This file (CK.Interop\Api\Win.cs) is part of CiviKey. 
+* This file (CK.Windows.Interop\Win.cs) is part of CiviKey. 
 *  
 * CiviKey is free software: you can redistribute it and/or modify 
 * it under the terms of the GNU Lesser General Public License as published 
