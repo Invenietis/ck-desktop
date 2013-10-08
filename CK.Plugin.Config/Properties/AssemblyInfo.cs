11ce6c78f1a1cca0ee1f37e0b578ef1bd0a0deb3
Cédric Legendre <cedric.legendre@invenietis.com>
2013-10-08 14:55:08 +0200

    Everybody in 2.9.13

--- CK.Plugin.Config/Properties/AssemblyInfo.cs
+++ CK.Plugin.Config/Properties/AssemblyInfo.cs
@@ -59,8 +59,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.9.13")]
+[assembly: AssemblyFileVersion("2.9.13")]
 
 // Allow CK.Tests assembly to acces to Internals of CK.Plugin.Config.
 // Here to ease the set up of NUnit tests.
